adf50b0fb72f4e35c6eacc17b80d92680f13d8c5
jhayX <lordsylharx@gmail.com>
2019-11-05 13:51:53 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyBmoL5ptVCNt3DaRvwX4xXLA9ubxqPIHdU",
+  authDomain: "jhayx-79000.firebaseapp.com",
+  databaseURL: "https://jhayx-79000.firebaseio.com",
+  projectId: "jhayx-79000",
+  storageBucket: "jhayx-79000.appspot.com",
+  messagingSenderId: "12166473847",
+  appId: "1:12166473847:web:c2b3181c06cf1fea1dbe72",
+  measurementId: "G-34082L9BZD"
 };
